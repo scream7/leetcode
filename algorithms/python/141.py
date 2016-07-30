b643a4ed57a74ec206f3b2c3777dcad6ea7ddbc1
Minjie Han <njhmj820@gmail.com>
2016-07-30 21:22:33 +0800

    Q141 python

---
+++ algorithms/python/141.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None:
+            return False
+        slow = head
+        fast = head.next if head else None
+        while fast and fast.next:
+            if slow == fast:
+                return True
+            else:
+                slow = slow.next
+                fast = fast.next.next
+        return False
+        
