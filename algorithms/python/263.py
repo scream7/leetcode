8d26c2e10cdd08c32a949b58eb26bc9ebf306609
Minjie Han <njhmj820@gmail.com>
2016-08-09 14:57:00 +0800

    Q263 python

---
+++ algorithms/python/263.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 1:
+            return True
+        elif num < 1:
+            return False
+        else:
+            while num>1:
+                if num % 2 == 0:
+                    num /= 2
+                elif num % 3 ==0:
+                    num /=3
+                elif num % 5 == 0:
+                    num /= 5
+                else:
+                    return False
+        return True
+        
